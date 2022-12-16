93eeddd00d2e3ef080bc6661c762ca958cb10379
Aadesh Bhardwaj <50766023+AadeshBhardwaj@users.noreply.github.com>
2022-12-16 08:10:01 +0530

    Update stop_script.sh

--- scripts/stop_script.sh
+++ scripts/stop_script.sh
@@ -5,6 +5,6 @@ if [ $pid == 0 ]; then
 else
    echo "Application is running"
    cd /home/ubuntu/node-express-realworld-example-app
-   pm2 stop app.js
+   sudo pm2 stop app.js
    echo "Application is stopped"
 fi
